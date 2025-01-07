87ba6a22b505d304805872557a65c4b728120b60
Yehor Radchenko <233542@esher.ac.uk>
2025-01-07 16:17:21 +0000

    editorrrrrr

---
+++ planimals/Forms/Editor.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Forms;
+
+namespace planimals.Forms
+{
+    public partial class Editor : Form
+    {
+
+
+        public Editor()
+        {
+            InitializeComponent();
+        }
+    }
+}
