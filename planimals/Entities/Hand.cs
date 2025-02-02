aa7ef449b282b46e1e10902fcdd678d2cb011ad5
Yehor Radchenko <233542@esher.ac.uk>
2025-02-02 20:37:04 +0000

    RAUUUGH

--- planimals/Entities/Hand.cs
+++ planimals/Entities/Hand.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Drawing;
 using System.IO;
-using System.Windows.Forms;
 
 public class Hand : List<Card>
 {
--- planimals/Forms/ResetPassword.cs
+++ planimals/Forms/ResetPassword.cs
@@ -102,4 +102,3 @@ public partial class ResetPassword : Form
         }
     }
 }
-
